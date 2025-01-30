a8bd867498a3b01dad417e4c4754ab7ef5bc8c16
Cal <airacolleen@gmail.com>
2025-01-30 13:41:36 +0800

    Landing Page 1.0
    -Moodify Text
    -Get Started Text
    -BG

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,25 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, ActivityIndicator } from "react-native";
+import { useFonts } from "expo-font";
+import { LeagueSpartan_400Regular, LeagueSpartan_700Bold } from "@expo-google-fonts/league-spartan";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    LeagueSpartanRegular: LeagueSpartan_400Regular,
+    LeagueSpartanBold: LeagueSpartan_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 justify-center items-center bg-[#EEEED0]"  // Tailwind background color
     >
-      <Text className="font-bold text-lg my-10">Moodify</Text>
+      <Text className="text-[#FF6B35] font-extrabold text-[55px] my-10 mt-60 tracking-tighter" style={{ fontFamily: "LeagueSpartanBold" }}>
+        Moodify
+      </Text>
       <Link href="/on-boarding-page1">Get Started</Link>
     </View>
   );
