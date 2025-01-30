ec5d9ff54d5a4629e40f04feee92d8880fc48ce7
Cal <airacolleen@gmail.com>
2025-01-30 15:49:20 +0800

    Landing Page 1.1
    -Import Images
    -Fix text and button

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { Text, View, ActivityIndicator } from "react-native";
 import { useFonts } from "expo-font";
 import { LeagueSpartan_400Regular, LeagueSpartan_700Bold } from "@expo-google-fonts/league-spartan";
 
+
 export default function Index() {
   const [fontsLoaded] = useFonts({
     LeagueSpartanRegular: LeagueSpartan_400Regular,
@@ -17,10 +18,16 @@ export default function Index() {
     <View
       className="flex-1 justify-center items-center bg-[#EEEED0]"  // Tailwind background color
     >
-      <Text className="text-[#FF6B35] font-extrabold text-[55px] my-10 mt-60 tracking-tighter" style={{ fontFamily: "LeagueSpartanBold" }}>
+      <Text className="text-[#FF6B35] font-extrathin text-[55px] mt-60 tracking-tighter" style={{ fontFamily: "LeagueSpartanBold" }}>
         Moodify
       </Text>
-      <Link href="/on-boarding-page1">Get Started</Link>
+      <Text> 
+      Your Journey to Well-being
+      </Text>
+      <Text>
+      One Mood at a Time
+      </Text>
+      <Link className="mt-10" href="/on-boarding-page1">Get Started</Link>
     </View>
   );
 }
