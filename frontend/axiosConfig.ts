ca53a4fb8019cfee72b17946b4a386bc3322076b
Raye Kyla <glean2120674@mkt.ceu.edu.ph>
2025-04-12 03:31:47 +0800

    fixed ip and fixing gitignore

--- frontend/axiosConfig.ts
+++ frontend/axiosConfig.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 // Replace with your ngrok/public/local server URL
-const BASE_URL = "http://192.168.1.7:3000"; 
+const BASE_URL = "http://localhost:3000"; 
 
 // "http://localhost:3000"; sa postman ganto
-//lagay mo local ip add mo NYHAHAHA like http://192.162.1.5:3000
+//lagay mo local ip add mo NYHAHAHA like http://192.162.1.5:3000 sa line 4
+
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
