53982779d7335024049752a9542d559473a387dd
Cal <airacolleen@gmail.com>
2025-04-13 23:38:15 +0800

    WORKING INTEG

--- frontend/axiosConfig.ts
+++ frontend/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Replace with your ngrok/public/local server URL
-const BASE_URL = "http://192.168.100.22"; 
+const BASE_URL = "http://192.168.100.22:3000"; 
 
 // "http://localhost:3000"; sa postman ganto
 //lagay mo local ip add mo NYHAHAHA like http://192.162.1.5:3000 sa line 4 para matest integ
